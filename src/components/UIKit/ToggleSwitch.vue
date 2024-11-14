e424f2451b79f55a3aa79af053c927cff3ebff51
amastyaev <amastyaev@sigma-it.ru>
2024-11-14 15:22:16 +0300

    feat: сделал компонент ProductFilter

--- src/components/UIKit/ToggleSwitch.vue
+++ src/components/UIKit/ToggleSwitch.vue
@@ -41,7 +41,7 @@ export default {
 <style lang="scss" scoped>
 .toggle {
     position: relative;
-    width: 65px;
+    width: 36px;
     height: 24px;
     cursor: pointer;
 }
@@ -51,7 +51,6 @@ export default {
 }
 
 .toggle__switch {
-    width: 36px;
     height: 22px;
     background-color: rgba(242, 242, 242, 1);
     border-radius: 12px;
